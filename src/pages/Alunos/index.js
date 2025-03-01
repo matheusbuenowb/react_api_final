71ec50c55967786d40bdc2456847976af9a2e01f
Matheus Bueno <matheus.faria_wb@hotmail.com>
2025-03-01 11:39:55 -0300

    Adicionado uma rota para realizar um get

--- src/pages/Alunos/index.js
+++ src/pages/Alunos/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
 import { Container } from '../../styles/GlobalStyles';
-import axios from '../..'
+import axios from '../../services/axios';
 
 export default function Alunos(){
+
+  React.useEffect(() => {
+    async function getData(){
+      const response = await axios.get('/alunos');
+      console.log(response.data);
+
+    }
+
+    getData();
+  }, []);
 
   return(
     <Container>

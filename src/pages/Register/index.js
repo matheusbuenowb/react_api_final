04710dfe3291f259841b412e03e2a264902634b5
Matheus Bueno <matheus.faria_wb@hotmail.com>
2025-03-06 17:54:46 -0300

    Criado componentes nome, email e senha para a tela de criar conta

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,13 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import { Container } from '../../styles/GlobalStyles';
-
+import {Form} from './styled';
 
 export default function Register(){
 
+  const [email, setEmail] = useState('');
+  const [nome, setNome] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSubmit(e){
+    e.preventDefault();
+
+    console.log(nome);
+  }
+
   return(
     <Container>
-    <h1>Login</h1>
+        <h1>Crie sua conta</h1>
+        <Form onSubmit={handleSubmit}>
+          <label htmlFor="nome">
+            Nome:
+            <input
+              type="text"
+              value = {nome}
+              onChange={e => setNome(e.target.value)}
+              placeholder="Seu nome"
+            />
+          </label>
+          <label htmlFor="email">
+            Email:
+            <input
+              type="email"
+              value = {email}
+              onChange={e => setEmail(e.target.value)}
+              placeholder="Seu email"
+            />
+          </label>
+          <label htmlFor="password">
+            Password:
+            <input
+              type="password"
+              value = {password}
+              onChange={e => setPassword(e.target.value)}
+              placeholder="Sua password"
+            />
+          </label>
+
+          <button type ="submit"> Criar minha conta</button>
+        </Form>
+
     </Container>
   )
 }
